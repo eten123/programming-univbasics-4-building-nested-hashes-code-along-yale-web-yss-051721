9778ee7bd0aa8d68a241ee8da9c422bf27fb532d
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-31 15:55:19 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -4,19 +4,15 @@ def third_challenge
    {
     :patriarch => {:name => "Lord Montague",:age => "53"},
     :matriarch => {:name => "Lady Montague",:age => "54"},
-    :hero => {:name => "Romeo",:age => "15"},
+    :hero => {:name => "Romeo",:age => "15", :status => "alive"},
     :hero_friends => "alive"
    }, 
    :capulet => 
    {
     :patriarch => {:name => "Lord capulet",:age => "50l"},
     :matriarch => {:name => "Lady Capulet",:age => "51"},
-    :heroine => {
-        #your key/value pairs here
-        :name => "Juliet",
-        :age => "15"
-        },
-      :heroine_friends => "alive"
+    :heroine => {:name => "Juliet",:age => "15", :status => "alive"},
+    :heroine_friends => "alive"
    }
   }
 
