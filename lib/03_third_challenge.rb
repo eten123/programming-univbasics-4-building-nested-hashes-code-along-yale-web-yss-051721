b97249fdbdc041d4c2efc93d4bde32a934379663
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-31 15:53:45 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,36 +1,17 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-        #your key/value pairs here
-        :name => "Lord Montague",
-        :age => "53"
-        },
-      :matriarch => {
-        #your key/value pairs here
-        :name => "Lady Montague",
-        :age => "54"
-        
-        },
-      :hero => {
-        :name => "Romeo",
-        :age => "15"
-        #your key/value pairs here
-        },
-      :hero_friends => "alive"
+   :montague => 
+   {
+    :patriarch => {:name => "Lord Montague",:age => "53"},
+    :matriarch => {:name => "Lady Montague",:age => "54"},
+    :hero => {:name => "Romeo",:age => "15"},
+    :hero_friends => "alive"
    }, 
-   :capulet => {
-      :patriarch => {
-        #your key/value pairs here
-        :name => "Lord capulet",
-        :age => "50l"
-        },
-      :matriarch => {
-        #your key/value pairs here
-        :name => "Lady Capulet",
-        :age => "51"
-        },
-      :heroine => {
+   :capulet => 
+   {
+    :patriarch => {:name => "Lord capulet",:age => "50l"},
+    :matriarch => {:name => "Lady Capulet",:age => "51"},
+    :heroine => {
         #your key/value pairs here
         :name => "Juliet",
         :age => "15"
